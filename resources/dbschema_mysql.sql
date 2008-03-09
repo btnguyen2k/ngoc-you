dc295b18f31eb2f83ec85a0e62c49e274fc98bcd
btnguyen2k <btnguyen2k@5fe8b82d-1943-0410-bacd-e11af7097589>
2008-03-09 07:15:22 +0000

    * Updated database schema * Updated schedule/plan status

--- resources/dbschema_mysql.sql
+++ resources/dbschema_mysql.sql
@@ -1,4 +1,5 @@
 DROP TABLE IF EXISTS nyconfig;
+DROP TABLE IF EXISTS nylocation;
 DROP TABLE IF EXISTS nygroup;
 DROP TABLE IF EXISTS nyupload;
 DROP TABLE IF EXISTS nyentry;
@@ -14,6 +15,79 @@ INSERT INTO nyconfig VALUES ('SITE_NAME', 'NgocYou');
 INSERT INTO nyconfig VALUES ('SITE_TITLE', 'www.you.com.vn');
 INSERT INTO nyconfig VALUES ('SITE_KEYWORDS', 'NgocYou Keywords');
 INSERT INTO nyconfig VALUES ('SITE_DESCRIPTION', 'NgocYou Description');
+INSERT INTO nyconfig VALUES ('MAX_UPLOAD_FILES', '5');
+INSERT INTO nyconfig VALUES ('MAX_UPLOAD_SIZE', '1000000');
+INSERT INTO nyconfig VALUES ('ALLOWED_UPLOAD_FILE_TYPES', '.gif .png .jpg');
+
+CREATE TABLE nylocation (
+	lid						INTEGER			NOT NULL AUTO_INCREMENT,
+	lname					VARCHAR(64)		NOT NULL,
+	PRIMARY KEY (lid)
+) ENGINE=InnoDb;				  
+INSERT INTO nylocation (lid, lname) VALUES (1, 'An Giang');
+INSERT INTO nylocation (lid, lname) VALUES (2, 'Bà Rịa Vũng Tàu');
+INSERT INTO nylocation (lid, lname) VALUES (3, 'Bắc Giang');
+INSERT INTO nylocation (lid, lname) VALUES (4, 'Bắc Kạn');
+INSERT INTO nylocation (lid, lname) VALUES (5, 'Bạc Liêu');
+INSERT INTO nylocation (lid, lname) VALUES (6, 'Bắc Ninh');
+INSERT INTO nylocation (lid, lname) VALUES (7, 'Bến Tre');
+INSERT INTO nylocation (lid, lname) VALUES (8, 'Bình Định');
+INSERT INTO nylocation (lid, lname) VALUES (9, 'Bình Dương');
+INSERT INTO nylocation (lid, lname) VALUES (10, 'Bình Phước');
+INSERT INTO nylocation (lid, lname) VALUES (11, 'Bình Thuận');
+INSERT INTO nylocation (lid, lname) VALUES (12, 'Cà Mau');
+INSERT INTO nylocation (lid, lname) VALUES (13, 'Cần Thơ');
+INSERT INTO nylocation (lid, lname) VALUES (14, 'Cao Bằng');
+INSERT INTO nylocation (lid, lname) VALUES (15, 'Đà Nẵng');
+INSERT INTO nylocation (lid, lname) VALUES (16, 'Đắc Lắc');
+INSERT INTO nylocation (lid, lname) VALUES (17, 'Đắc Nông');
+INSERT INTO nylocation (lid, lname) VALUES (18, 'Điện Biên');
+INSERT INTO nylocation (lid, lname) VALUES (19, 'Đồng Nai');
+INSERT INTO nylocation (lid, lname) VALUES (20, 'Đồng Tháp');
+INSERT INTO nylocation (lid, lname) VALUES (21, 'Gia Lai');
+INSERT INTO nylocation (lid, lname) VALUES (22, 'Hà Giang');
+INSERT INTO nylocation (lid, lname) VALUES (23, 'Hà Nam');
+INSERT INTO nylocation (lid, lname) VALUES (24, 'Hà Nội');
+INSERT INTO nylocation (lid, lname) VALUES (25, 'Hà Tây');
+INSERT INTO nylocation (lid, lname) VALUES (26, 'Hà Tĩnh');
+INSERT INTO nylocation (lid, lname) VALUES (27, 'Hải Dương');
+INSERT INTO nylocation (lid, lname) VALUES (28, 'Hải Phòng');
+INSERT INTO nylocation (lid, lname) VALUES (29, 'Hậu Giang');
+INSERT INTO nylocation (lid, lname) VALUES (30, 'Hoà Bình');
+INSERT INTO nylocation (lid, lname) VALUES (31, 'Hưng Yên');
+INSERT INTO nylocation (lid, lname) VALUES (32, 'Khánh Hoà');
+INSERT INTO nylocation (lid, lname) VALUES (33, 'Kiên Giang');
+INSERT INTO nylocation (lid, lname) VALUES (34, 'Kon Tum');
+INSERT INTO nylocation (lid, lname) VALUES (35, 'Lai Châu');
+INSERT INTO nylocation (lid, lname) VALUES (36, 'Lâm Đồng');
+INSERT INTO nylocation (lid, lname) VALUES (37, 'Lạng Sơn');
+INSERT INTO nylocation (lid, lname) VALUES (38, 'Lào Cai');
+INSERT INTO nylocation (lid, lname) VALUES (39, 'Long An');
+INSERT INTO nylocation (lid, lname) VALUES (40, 'Nam Định');
+INSERT INTO nylocation (lid, lname) VALUES (41, 'Nghệ An');
+INSERT INTO nylocation (lid, lname) VALUES (42, 'Ninh Bình');
+INSERT INTO nylocation (lid, lname) VALUES (43, 'Ninh Thuận');
+INSERT INTO nylocation (lid, lname) VALUES (44, 'Phú Thọ');
+INSERT INTO nylocation (lid, lname) VALUES (45, 'Phú Yên');
+INSERT INTO nylocation (lid, lname) VALUES (46, 'Quảng Bình');
+INSERT INTO nylocation (lid, lname) VALUES (47, 'Quảng Nam');
+INSERT INTO nylocation (lid, lname) VALUES (48, 'Quảng Ngãi');
+INSERT INTO nylocation (lid, lname) VALUES (49, 'Quảng Ninh');
+INSERT INTO nylocation (lid, lname) VALUES (50, 'Quảng Trị');
+INSERT INTO nylocation (lid, lname) VALUES (51, 'Sóc Trăng');
+INSERT INTO nylocation (lid, lname) VALUES (52, 'Sơn La');
+INSERT INTO nylocation (lid, lname) VALUES (53, 'TP Hồ Chí Minh');
+INSERT INTO nylocation (lid, lname) VALUES (54, 'Tây Ninh');
+INSERT INTO nylocation (lid, lname) VALUES (55, 'Thái Bình');
+INSERT INTO nylocation (lid, lname) VALUES (56, 'Thái Nguyên');
+INSERT INTO nylocation (lid, lname) VALUES (57, 'Thanh Hoá');
+INSERT INTO nylocation (lid, lname) VALUES (58, 'Thừa thiên Huế');
+INSERT INTO nylocation (lid, lname) VALUES (59, 'Tiền Giang');
+INSERT INTO nylocation (lid, lname) VALUES (60, 'Trà Vinh');
+INSERT INTO nylocation (lid, lname) VALUES (61, 'Tuyên Quang');
+INSERT INTO nylocation (lid, lname) VALUES (62, 'Vĩnh Long');
+INSERT INTO nylocation (lid, lname) VALUES (63, 'Vĩnh Phúc');
+INSERT INTO nylocation (lid, lname) VALUES (64, 'Yên Bái');
 
 CREATE TABLE nygroup (
 	gid						INTEGER			NOT NULL,
@@ -73,6 +147,13 @@ CREATE TABLE nyentry (
 	ebody					TEXT,
 	enumviews				INTEGER			NOT NULL DEFAULT 0,
 		INDEX (enumviews),
+	elocation				INTEGER,
+		INDEX (elocation),
+	eprice					DOUBLE,
+		INDEX (eprice),
+	etype					TINYINT			NOT NULL DEFAULT 0,
+		INDEX (etype),
+	ehtml					TINYINT			NOT NULL DEFAULT 0,
 	PRIMARY KEY(eid)
 ) ENGINE=InnoDB;
 
