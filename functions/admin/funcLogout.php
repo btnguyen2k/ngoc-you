08b94dc4ed036c312bea00c095158255c1766f65
btnguyen2k <btnguyen2k@5fe8b82d-1943-0410-bacd-e11af7097589>
2008-03-13 10:32:55 +0000

    * Fixed #NGOCYOU-27: logging out from Administration section now brings user to the index page.

--- functions/admin/funcLogout.php
+++ functions/admin/funcLogout.php
@@ -2,5 +2,5 @@
 require_once 'includes/denyDirectInclude.php';
 require_once 'includes/config.php';
 unset($_SESSION[SESSION_CURRENT_USER_ID]);
-header('Location: admin.php');
+header('Location: index.php');
 ?>
